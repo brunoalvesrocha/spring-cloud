ab4d535de621a4c0a0b3ce6eb6990e9c64034f34
Bruno Rocha <brunoalvesrocha@icloud.com>
2018-02-16 09:14:57 -0200

    add-zull-service-properties

--- eureka-service/src/main/java/com/techprimers/stock/eurekaservice/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/techprimers/stock/eurekaservice/EurekaServiceApplication.java
@@ -16,6 +16,7 @@ import org.springframework.context.annotation.Configuration;
 
 @EnableEurekaServer
 @SpringBootApplication
+
 public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
